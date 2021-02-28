a61ce7f69f54e0df22a407fad358636181599734
Rohit Pradhan <rohit.p.pradhan88@gmail.com>
2021-02-28 18:19:47 -0500

    Moved helpers to separate file

--- src/executor/execute.go
+++ src/executor/execute.go
@@ -2,7 +2,6 @@
 package executor
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 )
@@ -16,7 +15,7 @@ import (
 // Requirements:
 // procname musut be callable from the command line.
 // If a command produces an error then it will be returned from Execute.
-func Execute(procname string, commands [][]string) (error) {
+func Execute(procname string, commands [][]string) error {
   for _, command := range commands {
     cmd := exec.Command(procname, command...)
     // Pipe output & input to terminal
@@ -28,32 +27,8 @@ func Execute(procname string, commands [][]string) (error) {
   return nil
 }
 
-// printError prints the error with the information given.
-// procname and command are printed to tell the user which command
-// returned an error code, and err is the information from stderr
-// regarding the error.
-func printError(procname string, command []string, err string) {
-  fmt.Println("gsp failed at the following command:")
-  // Make the command into a better-looking string
-  s := procname + " "
-  for i := 0; i < len(command); i++ {
-    s += command[i] + " "
-  }
-  fmt.Println(s)
-  fmt.Println("Received message from stderr:")
-  fmt.Println(err)
-}
-
-// commandToString converts command to a string, where each element
-// in command is separated by a whitespace (since terminal options are
-// separated by whitespace). The first word in the value returned by 
-// comandToString is procname.
-func commandToString(procname string, command []string) string {
-  s := procname + " "
-  len := len(command)
-  for i := 0; i < len - 1; i++ {
-    s += command[i] + " "
-  }
-  s += command[len-1]
-  return s
+// Executable returns 'true' if a command-line command can be executed.
+func Executable(procname string) bool {
+  _, err := exec.LookPath(procname)
+  return err == nil
 }
---
+++ src/executor/helpers.go
@@ -0,0 +1,34 @@
+package executor
+import (
+  "fmt"
+)
+
+// printError prints the error with the information given.
+// procname and command are printed to tell the user which command
+// returned an error code, and err is the information from stderr
+// regarding the error.
+func printError(procname string, command []string, err string) {
+  fmt.Println("gsp failed at the following command:")
+  // Make the command into a better-looking string
+  s := procname + " "
+  for i := 0; i < len(command); i++ {
+    s += command[i] + " "
+  }
+  fmt.Println(s)
+  fmt.Println("Received message from stderr:")
+  fmt.Println(err)
+}
+
+// commandToString converts command to a string, where each element
+// in command is separated by a whitespace (since terminal options are
+// separated by whitespace). The first word in the value returned by 
+// comandToString is procname.
+func commandToString(procname string, command []string) string {
+  s := procname + " "
+  len := len(command)
+  for i := 0; i < len - 1; i++ {
+    s += command[i] + " "
+  }
+  s += command[len-1]
+  return s
+}
